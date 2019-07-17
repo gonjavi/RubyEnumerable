c816316f6a4f865c7357ccbed8859bb9f5400764
gonjavi <javiercaliescali@gmail.com>
2019-07-17 15:51:54 -0500

    my each

--- enumerable.rb
+++ enumerable.rb
@@ -5,17 +5,9 @@ module Enumerable
           yield (object)
        end
     end
-    def my_each_with_index
-      array.length.times do |i| 
-        object=array[i]  
-          yield (object, i)
-       end
-    end
+ 
 
  
 end
 
 
-my_each([1,3,4,5,6,7,8]) do |x|
-  puts x
-end
