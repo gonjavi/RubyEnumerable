964aad72bc91c8560d64142f12f3926c6e739c3e
gonjavi <javiercaliescali@gmail.com>
2019-07-17 15:47:00 -0500

    second

---
+++ enumerable.rb
@@ -0,0 +1,21 @@
+module Enumerable
+    def my_each
+       array.length.times do |i| 
+        obtect=array[i]  
+          yield (object)
+       end
+    end
+    def my_each_with_index
+      array.length.times do |i| 
+        object=array[i]  
+          yield (object, i)
+       end
+    end
+
+ 
+end
+
+
+my_each([1,3,4,5,6,7,8]) do |x|
+  puts x
+end
